adcad78fccf9599edcd8813b74bcb571344a40c3
lugDroid <diazbastos@gmail.com>
2019-05-22 13:37:35 +0200

    Numerical buttons fixed

--- js/main.js
+++ js/main.js
@@ -1,59 +1,89 @@
-// cache buttons
-const button0 = document.getElementById('button-0')
-const btn1 = document.getElementById('button-1')
-const btn2 = document.getElementById('button-2')
-const btn3 = document.getElementById('button-3')
-const btn4 = document.getElementById('button-4')
-const btn5 = document.getElementById('button-5')
-const btn6 = document.getElementById('button-6')
-const btn7 = document.getElementById('button-7')
-const btn8 = document.getElementById('button-8')
-const btn9 = document.getElementById('button-9')
+/* 
+TODO
+- Prevent display from showing more than a decimal point
+- Prevent display from showing leading 0
+*/
 
-// process button clicks
+// cache elements
+const display = document.getElementById('display')
+const button0 = document.getElementById('button-0')
+const button1 = document.getElementById('button-1')
+const button2 = document.getElementById('button-2')
+const button3 = document.getElementById('button-3')
+const button4 = document.getElementById('button-4')
+const button5 = document.getElementById('button-5')
+const button6 = document.getElementById('button-6')
+const button7 = document.getElementById('button-7')
+const button8 = document.getElementById('button-8')
+const button9 = document.getElementById('button-9')
+const btnDecPoint = document.getElementById('button-mark')
+
+// button event listeners
 button0.addEventListener('click', event => {
-  numberPressed("0", "#display")
+  displayNumber("0", display)
 })
+
+button1.addEventListener('click', event => {
+  displayNumber("1", display)
+})
+
+button2.addEventListener('click', event => {
+  displayNumber("2", display)
+})
+
+button3.addEventListener('click', event => {
+  displayNumber("3", display)
+})
+
+button4.addEventListener('click', event => {
+  displayNumber("4", display)
+})
+
+button5.addEventListener('click', event => {
+  displayNumber("5", display)
+})
+
+button6.addEventListener('click', event => {
+  displayNumber("6", display)
+})
+
+button7.addEventListener('click', event => {
+  displayNumber("7", display)
+})
+
+button8.addEventListener('click', event => {
+  displayNumber("8", display)
+})
+
+button9.addEventListener('click', event => {
+  displayNumber("9", display)
+})
+
+btnDecPoint.addEventListener('click', event => {
+  displayNumber(".", display)
+})
+
+// functions
+function displayNumber(number, display) {
+  // if we've finished a calculation clear display for new number 
+  // else add current number to existing ones
+  let displayContents = display.innerHTML;
+
+   if (endValue != true) {
+    if (displayContents.length < 13) {
+      display.innerHTML = displayContents + number;
+    }
+  } else {
+    display.innerHTML = number;
+    endValue = false;
+  }
+}
+
+
+
+
+
   /* number buttons */ /*
-  $('#button-0').click(function() {
-    numberPressed("0", "#display");
-  });
-  
-  $('#button-1').click(function() {
-    numberPressed("1", "#display");
-  });
-  
-  $('#button-2').click(function() {
-    numberPressed("2", "#display");
-  });
-  
-  $('#button-3').click(function() {
-    numberPressed("3", "#display");
-  });
-  
-  $('#button-4').click(function() {
-    numberPressed("4", "#display");
-  });
-  
-  $('#button-5').click(function() {
-    numberPressed("5", "#display");
-  });
-  
-  $('#button-6').click(function() {
-    numberPressed("6", "#display");
-  });
-  
-  $('#button-7').click(function() {
-    numberPressed("7", "#display");
-  });
-  
-  $('#button-8').click(function() {
-    numberPressed("8", "#display");
-  });
-  
-  $('#button-9').click(function() {
-    numberPressed("9", "#display");
-  });
   
   $('#button-mark').click(function() {
     numberPressed(".", '#display');
@@ -109,24 +139,7 @@ var endValue = false;
 var operand = null;
 
 /* aux functions */
-function numberPressed(number, element) {
-  // if we've finished a calculation clear display for new number 
-  // else add current number to existing ones
-  console.log(number)
-  console.log(element)
-  element.innerHTML = number
 
-  /*var displayContents = element.innerHTML;
-
-   if (endValue != true || displayContents != undefined) {
-    //if (displayContents.length < 13) {
-      element.innerHTML = displayContents + number;
-    //}
-  } else {
-    element.innerHTML = number;
-    endValue = false;
-  } */
-}
 
 function opPressed(op) {
   // if it's the first operation store value
